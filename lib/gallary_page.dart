37efe0337b15e5b304a5cc8cfb6d6f91c8687af5
Bvin <bvin@live.cn>
2020-03-30 14:44:46 +0800

    去除加载框

--- lib/gallary_page.dart
+++ lib/gallary_page.dart
@@ -22,18 +22,6 @@ class PageState extends State<GalleryPage>{
       child: PhotoViewGallery.builder(
           pageController: PageController(initialPage: widget.position),
           itemCount: widget.photos.length,
-          loadingBuilder: (context, event) => Center(
-            child: Container(
-              width: 80.0,
-              height: 80.0,
-              child: CircularProgressIndicator(
-                value: event == null
-                    ? 0
-                    : event.cumulativeBytesLoaded / event.expectedTotalBytes,
-                backgroundColor: Colors.blue,
-              ),
-            ),
-          ),
           builder: (BuildContext context, int index) {
             var url = widget.photos[index]["url"];
             url = "https://gallery.1x.com" + url.replaceAll("ld", "hd4");
