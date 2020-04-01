66a3500972c1d2f79621498c0bec109acd1d25b2
Bvin <bvin@live.cn>
2020-04-01 10:36:11 +0800

    添加底部导航栏

--- lib/main.dart
+++ lib/main.dart
@@ -39,6 +39,11 @@ class PageState extends State<HomePage> with SingleTickerProviderStateMixin{
   Widget build(BuildContext context) {
     return Scaffold(
         body: body(context),
+        bottomNavigationBar: BottomNavigationBar(items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), title: Text("Home")),
+          BottomNavigationBarItem(icon: Icon(Icons.explore), title: Text("Explore")),
+          BottomNavigationBarItem(icon: Icon(Icons.person), title: Text("Mine")),
+        ]),
       );
   }
 
